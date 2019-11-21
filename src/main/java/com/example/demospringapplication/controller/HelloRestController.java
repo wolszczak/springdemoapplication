0b8366fe27722464100fd934ee7138f04d423cc6
612251 <612251@DTPE01L2P4.perdigao.com.br>
2019-11-21 09:42:15 -0200

    teste

--- src/main/java/com/example/demospringapplication/controller/HelloRestController.java
+++ src/main/java/com/example/demospringapplication/controller/HelloRestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 public class HelloRestController {
 	
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	@ResponseBody
 	public String hello() {
 		return "Hello from Spring!";
